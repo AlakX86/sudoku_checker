3e5b8d9aef46b43a23877c3efb2738309c4a761e
Aarón Silvoso García <148151768+AlakX86@users.noreply.github.com>
2023-11-02 11:31:05 +0100

    Fix small module error

--- check_sudoku.py
+++ check_sudoku.py
@@ -1,6 +1,6 @@
-from check_columns import check_columns
-from check_rows import check_rows
-from check_square import check_square
-from check_numbers import check_numbers
+from src.check_columns import check_columns
+from src.check_rows import check_rows
+from src.check_square import check_square
+from src.check_numbers import check_numbers
 def check_sudoku(sudoku):
     return check_columns(sudoku) and check_rows(sudoku) and check_numbers(sudoku) and check_square(sudoku)
